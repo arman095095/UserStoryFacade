c9b8826586b0c36df0d327a93bcf15729d8fc7c3
Арман Чархчян <re-mont1@mail.ru>
2022-05-05 14:31:38 +0400

    fix

--- Sources/UserStoryFacade/UserStoryFacade.swift
+++ Sources/UserStoryFacade/UserStoryFacade.swift
@@ -7,13 +7,25 @@ import AuthorizationRouteMap
 import SelectionRouteMap
 import AuthorizedZoneRouteMap
 
-public final class UserStoryFacade {
-    public init() { }
-    public var postsUserStory: PostsRouteMap?
-    public var profileUserStory: ProfileRouteMap?
-    public var settingsUserStory: SettingsRouteMap?
+public protocol UserStoryFacadeProtocol: AnyObject {
+    var postsUserStory: PostsRouteMap? { set get }
+    var profileUserStory: ProfileRouteMap? { set get }
+    var settingsUserStory: SettingsRouteMap? { set get }
+    var authorizationUserStory: AuthorizationRouteMap? { set get }
+    var accountUserStory: AccountRouteMap? { set get }
+    var regionUserStory: RegionSelectionRouteMap? { set get }
+    var authorizedZoneUserStory: AuthorizedZoneRouteMap? { set get }
+}
+
+public final class UserStoryFacade: UserStoryFacadeProtocol {
+    
     public var authorizationUserStory: AuthorizationRouteMap?
     public var accountUserStory: AccountRouteMap?
     public var regionUserStory: RegionSelectionRouteMap?
+    public var postsUserStory: PostsRouteMap?
+    public var profileUserStory: ProfileRouteMap?
+    public var settingsUserStory: SettingsRouteMap?
     public var authorizedZoneUserStory: AuthorizedZoneRouteMap?
+
+    public init() { }
 }
