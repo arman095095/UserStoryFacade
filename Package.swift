f88d248f59b5e753a57fe1e4529f1de4f3c48c6c
Арман Чархчян <re-mont1@mail.ru>
2022-05-05 19:48:58 +0400

    fix

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-private let remoteDependencies: [Package.Dependency] = [
+private let dependencies: [Package.Dependency] = [
     .package(url: "https://github.com/arman095095/SettingsRouteMap.git", branch: "develop"),
     .package(url: "https://github.com/arman095095/PostsRouteMap.git", branch: "develop"),
     .package(url: "https://github.com/arman095095/ProfileRouteMap.git", branch: "develop"),
@@ -12,19 +12,6 @@ private let remoteDependencies: [Package.Dependency] = [
     .package(url: "https://github.com/arman095095/AccountRouteMap.git", branch: "develop"),
     .package(url: "https://github.com/arman095095/AuthorizedZoneRouteMap.git", branch: "develop")
 ]
-
-private let localDependencies: [Package.Dependency] = [
-    .package(path: "/Users/armancarhcan/Desktop/Workdir/SettingsRouteMap"),
-    .package(path: "/Users/armancarhcan/Desktop/Workdir/PostsRouteMap"),
-    .package(path: "/Users/armancarhcan/Desktop/Workdir/ProfileRouteMap"),
-    .package(path: "/Users/armancarhcan/Desktop/Workdir/AuthorizationRouteMap"),
-    .package(path: "/Users/armancarhcan/Desktop/Workdir/SelectionRouteMap"),
-    .package(path: "/Users/armancarhcan/Desktop/Workdir/AccountRouteMap"),
-    .package(path: "/Users/armancarhcan/Desktop/Workdir/AuthorizedZoneRouteMap")
-]
-
-let isDev = true
-private let dependencie = isDev ? localDependencies : remoteDependencies
 
 let package = Package(
     name: "UserStoryFacade",
@@ -35,7 +22,7 @@ let package = Package(
             name: "UserStoryFacade",
             targets: ["UserStoryFacade"]),
     ],
-    dependencies: dependencie,
+    dependencies: dependencies,
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
